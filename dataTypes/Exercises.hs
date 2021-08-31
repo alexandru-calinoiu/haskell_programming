8099071c481feeef380d630897d48862d807a0c4
calin <calinoiu.alexandru@agilefreaks.com>
2021-08-31 20:09:13 +0300

    Add datatypes

---
+++ dataTypes/Exercises.hs
@@ -0,0 +1,14 @@
+module Exercises where
+
+awesome = ["Pauchon", "curry", ":)"]
+also = ["Quake", "The Simons"]
+allAwesome = [awesome, also]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = if x < 0 then negate x else x
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y = ((snd x, snd y), (fst x, fst y))
